c252ebaa1a9d69c5b6b92daa6715953521882fb5
Hari <kathulaharish929311@gmail.com>
2023-11-15 11:44:08 +0530

    routes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import bodyParser from 'body-parser';
 
 //db
 import Connection from './database/db.js';
+//routes
+import Routes from './routes/route.js'
 
 const app = express();
 
 dotenv.config()
 
-const PORT = 8000
+app.use(bodyParser.json({extended:true}))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+
+app.use('/', Routes)
+
+const PORT = 8080
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
